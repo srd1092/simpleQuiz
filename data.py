eead65117ad521c919ec01cc7b9ab6a6524214da
Soumyaranjan Dikshit <105819550+srd1092@users.noreply.github.com>
2023-07-12 14:50:14 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,52 @@
+# question_data = [
+# {"text": "A slug's blood is green.", "answer": "True"},
+# {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+# {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+# {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+# {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home
+# to eat.", "answer": "True"},
+# {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+# "answer": "False"},
+# {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+# {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+# {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+# {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+# {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+# {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+trivia_question_data = [
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Great Wall of China is visible from the moon.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Video streaming website YouTube was purchased in it&#039;s entirety by Facebook for US$1.65 billion "
+                 "in stock.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "French is an official language in Canada.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "It is automatically considered entrapment in the United States if the police sell you illegal "
+                 "substances without revealing themselves.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple in other languages.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Pluto is a planet.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "One of Donald Trump&#039;s 2016 Presidential Campaign promises was to build a border wall between "
+                 "the United States and Mexico.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Ping-Pong originated in England", "correct_answer": "True",
+     "incorrect_answers": ["False"]}
+]
